e48fac83b33f978876d2a9f2b90540b7d232c931
patrick Wiley <wileypatrick36@yahoo.com>
2019-09-15 20:36:41 +0200

    change outlets

--- RuckSack/Controller/ExchangeCell.swift
+++ RuckSack/Controller/ExchangeCell.swift
@@ -10,18 +10,15 @@ import UIKit
 
 class ExchangeCell: UITableViewCell {
     
-    @IBOutlet weak var exchangeView: UIView!
     @IBOutlet weak var imageCurrency: UIImageView!
     @IBOutlet weak var shortLabel: UILabel!
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var flagCurrency: UIImageView!
     @IBOutlet weak var amount: UITextField!
     
     func setCurrency(currency: Currency) {
         imageCurrency.image = currency.image
         shortLabel.text = currency.shortLabel
         name.text = currency.name
-        flagCurrency.image = currency.flag
         amount.text = currency.amount
         
         
